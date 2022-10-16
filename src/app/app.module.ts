cc2655890181c9d62278af6e8be3ee664fcf928d
ritobroto sarkar <c.sarkar1962@gmail.com>
2022-10-16 17:21:12 +0530

    custom service in angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { SocialLoginModule, SocialAuthServiceConfig } from '@abacritt/angularx-social-login';
+import {
+  GoogleLoginProvider,
+  FacebookLoginProvider
+} from '@abacritt/angularx-social-login';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GoogleloginComponent } from './googlelogin/googlelogin.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GoogleloginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SocialLoginModule
+  ],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '338382652536-bbbf1c1djnup9r5jobd4jnsihdr2i5rr.apps.googleusercontent.com'
+            )
+          },
+        ],
+        onError: (err) => {
+          console.error(err);
+        }
+      } as SocialAuthServiceConfig,
+    }
+
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/googlelogin/googlelogin.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { SocialAuthService } from "@abacritt/angularx-social-login";
+import { SocialUser } from "@abacritt/angularx-social-login";
+
+
+@Component({
+  selector: 'app-googlelogin',
+  templateUrl: './googlelogin.component.html',
+  styleUrls: ['./googlelogin.component.css']
+})
+export class GoogleloginComponent implements OnInit {
+  user: any;
+  loggedIn:any;
+
+  constructor(private authService: SocialAuthService) { }
+
+  ngOnInit(): void {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = (user != null);
+    });
+  }
+
+}
